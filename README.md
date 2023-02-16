938fb5c66374c4aef12e9799b9ff079cedd608b6
Alan Xie <axie5247@uni.sydney.edu.au>
2023-02-17 01:08:16 +1100

    Added content to readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # BomberWiz
+To play the game, run *gradle build run* on your terminal.
+
+## Control
+The character is controlled with up, down, left, and right arrow keys for movement and space key to drop a bomb.
+
+## Mechanism
+- Each game a player gets 3 lives, the player looses a live when it is hit by a bomb or interacted with an enemy.
+- Only brick blocks can be destroyed by a bomb.
+- The player looses when:
+  - The timer counts down to 0, where the timer starts with 180 seconds.
+  - All lives are lost.
+- Player progresses to the next level by reaching the goal block of the current level.
+- The player wins by reaching the goal block of the last level.  
+- There are currently 2 levels.
+- There are two enemies: Red and Yellow enemy. They can be identified by the color of their "core" and the orbs
+surrounding them. The enemies can be killed by bombs.
+  - Red enemy moves to a random direction when it hits a wall.
+  - Yellow enemy moves in a clockwise fashion. 
+
+## Issues
+- Tests are disabled for the *BombWiz* class because they are acting weird.
+
+# Future Plans
+- Fix the testing and use Mockito. Need sometime to comprehend the code written by my dumb first-year self...
+- Apply design patterns, the current structure is tedious.
+- Better graphic and design.
+- Add new features:
+  - More levels
+  - Be able to restart
+  - Have a backstory
+  - Enemies be more threatening
+  - An UI
--- src/test/java/bomberwiz/BombWizTest.java
+++ src/test/java/bomberwiz/BombWizTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
 
 import org.junit.jupiter.api.*;
+import processing.core.PApplet;
 
 public class BombWizTest {
     App app = new App();
@@ -15,63 +16,63 @@ public class BombWizTest {
     private static BombWiz test2;
     private static App app2 = new App();
 
-    @BeforeAll
-    public static void setup() {
-        test2 = new BombWiz(10, 10, app2, 10);
-    }
-
-    @Test
-    public void movementTest2(){
-        assertEquals(10, test2.getX());
-    }
+//    @BeforeAll
+//    public static void setup() {
+//        test2 = new BombWiz(10, 10, app2, 10);
+//    }
+//
+//    @Test
+//    public void movementTest2(){
+//        assertEquals(10, test2.getX());
+//    }
 
     // Test bombguy movement with keys.
-//    @Test
-//    public void movementTest(){
-//        testPlayer.move(39, testMap); //Right
-//        //32 80
-//        assertEquals(64, testPlayer.getX());
-//        assertEquals(80, testPlayer.getY());
-//        assertEquals("right", testPlayer.getDirectionString());
-//
-//        testPlayer.move(39, testMap); //Right
-//
-//        testPlayer.move(40, testMap); //Down
-//        assertEquals(96, testPlayer.getX());
-//        assertEquals(112, testPlayer.getY());
-//        assertEquals("down", testPlayer.getDirectionString());
-//
-//        testPlayer.move(40, testMap); //Down
-//
-//        testPlayer.move(37, testMap); //Left
-//        assertEquals(64, testPlayer.getX());
-//        assertEquals(144, testPlayer.getY());
-//        assertEquals("left", testPlayer.getDirectionString());
-//
-//        testPlayer.move(37, testMap); //Left
-//        assertEquals("left", testPlayer.getDirectionString());
-//
-//        testPlayer.move(38, testMap); //Up
-//        assertEquals(32, testPlayer.getX());
-//        assertEquals(112, testPlayer.getY());
-//        assertEquals("up", testPlayer.getDirectionString());
-//
-//        for(int i = 0; i < 120; i++){
-//            testPlayer.tick(testMap, null);
-//        }
-//    }
-//
-//    @BeforeEach
-//    public void setUp(){
-//        app.noLoop();
-//        app.setConfig("src/test/resources/config.json");
-//        PApplet.runSketch(new String[] {"App"}, app);
-//
-//        testMap = new Map(app);
-//        testMap.readConfig("src/test/resources/config.json");
-//        testMap.setMap(testMap.getLevels().get(0), testMap.getTimes().get(0));
-//        testMap.generateEnemies();
-//        testEnemies = testMap.getEnemies();
-//        testPlayer = new BombGuy(testMap.getPlayerLocX(), testMap.getPlayerLocY(), app, testMap.getPlayerLives());
-//    }
+    @Test
+    public void movementTest(){
+        testPlayer.move(39, testMap); //Right
+        //32 80
+        assertEquals(64, testPlayer.getX());
+        assertEquals(80, testPlayer.getY());
+        assertEquals("right", testPlayer.getDirectionString());
+
+        testPlayer.move(39, testMap); //Right
+
+        testPlayer.move(40, testMap); //Down
+        assertEquals(96, testPlayer.getX());
+        assertEquals(112, testPlayer.getY());
+        assertEquals("down", testPlayer.getDirectionString());
+
+        testPlayer.move(40, testMap); //Down
+
+        testPlayer.move(37, testMap); //Left
+        assertEquals(64, testPlayer.getX());
+        assertEquals(144, testPlayer.getY());
+        assertEquals("left", testPlayer.getDirectionString());
+
+        testPlayer.move(37, testMap); //Left
+        assertEquals("left", testPlayer.getDirectionString());
+
+        testPlayer.move(38, testMap); //Up
+        assertEquals(32, testPlayer.getX());
+        assertEquals(112, testPlayer.getY());
+        assertEquals("up", testPlayer.getDirectionString());
+
+        for(int i = 0; i < 120; i++){
+            testPlayer.tick(testMap, null);
+        }
+    }
+
+    @BeforeEach
+    public void setUp(){
+        app.noLoop();
+        app.setConfig("src/test/resources/config.json");
+        PApplet.runSketch(new String[] {"App"}, app);
+
+        testMap = new Map(app);
+        testMap.readConfig("src/test/resources/config.json");
+        testMap.setMap(testMap.getLevels().get(0), testMap.getTimes().get(0));
+        testMap.generateEnemies();
+        testEnemies = testMap.getEnemies();
+        testPlayer = new BombWiz(testMap.getPlayerLocX(), testMap.getPlayerLocY(), app, testMap.getPlayerLives());
+    }
 }
